c71ef36cf2b821c4a2f6d482561d468b47faa164
aj709 <55025205+aj709@users.noreply.github.com>
2020-09-04 01:18:25 +0530

    Updates

---
+++ precache-manifest.ff97b194cd96d28da9b3a17a0941b19d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc49ec0bd249c7837bdebb6f9c0c9c1",
+    "url": "/React-Image-Gallery/index.html"
+  },
+  {
+    "revision": "f4ba221a089aabb54c97",
+    "url": "/React-Image-Gallery/static/css/main.c2d120bc.chunk.css"
+  },
+  {
+    "revision": "f8facd7f25c834dfe854",
+    "url": "/React-Image-Gallery/static/js/2.9a9ada10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Image-Gallery/static/js/2.9a9ada10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ba221a089aabb54c97",
+    "url": "/React-Image-Gallery/static/js/main.9c52af64.chunk.js"
+  },
+  {
+    "revision": "c71cef6be37826b90d23",
+    "url": "/React-Image-Gallery/static/js/runtime-main.e8f6bf84.js"
+  }
+]);
